7b19c7163ae2511a21fb625d7168316d91b7db58
Tabasum Ali <120305067+TabasumAli@users.noreply.github.com>
2025-02-08 22:13:07 +0500

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -24,7 +24,7 @@ allow_web_search = st.checkbox("Allow Web Search")
 
 user_query = st.text_area("Enter your query: ", height=150, placeholder="Ask anything about space!")
 
-API_URL = "http://127.0.0.1:9999/chat"
+API_URL = "https://spaceassistant.onrender.com/chat"
 
 if st.button("Ask Agent!"):
     if user_query.strip():
